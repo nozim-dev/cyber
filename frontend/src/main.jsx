786ef54ba37dad28de208c32afa007e48709c1cb
nozim-dev <nozimfordevelop@gmail.com>
2024-11-06 11:03:47 +0500

    adding react-router-dom library in frontend folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./Pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeLayouts from "./Layouts/HomeLayouts";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayouts />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
